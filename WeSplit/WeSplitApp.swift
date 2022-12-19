9a197c1b97974d2003007c95ba6aba72fddb45b5
bektemur-07 <104037405+bektemur-07@users.noreply.github.com>
2022-12-19 11:02:28 +0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Bektemur on 19/12/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
